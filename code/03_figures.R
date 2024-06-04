3986365ec13143daa1e5df677d6ec40a0e9157fb
Enrique Acosta <kikepaila@gmail.com>
2024-06-04 22:48:59 +0200

    quick test ggbump

--- code/03_figures.R
+++ code/03_figures.R
@@ -32,3 +32,44 @@ codes <- read_csv("data_input/us_state_codes.csv")
              pch = 21) +
     scale_color_identity() +
     scale_fill_identity()
+
+  
+  ggsave("figures/test1.png",
+         w = 6, h = 10)
+  
+  
+  
+  tt <- 
+    cmp |> 
+    full_join(codes, by = join_by(state)) |> 
+    full_join(polit, by = join_by(state,phase)) |> 
+    filter(!is.na(phase)) |> 
+    mutate(governor = if_else(polit <= 2, "#3157d4","#db2a2a"),
+           governor = if_else(state == "Nebraska", "#db2a2a",governor),
+           governor = if_else(state == "District of Columbia", "#3157d4",governor),
+           chamber = case_when(polit == 1 ~ "#3157d450",
+                               polit == 2 ~ "#a618c950",
+                               polit == 3 ~ "#a618c950",
+                               polit == 4 ~ "#db2a2a50",
+                               TRUE ~ "#a618c950"))
+  
+  
+  
+  library(ggbump)
+  
+  tt |> 
+    ggplot(aes(x = phase, y = rnk_std-1, group = state)) +
+    geom_bump(alpha = .5) +
+    theme_minimal() +
+    geom_point(color = "white",
+               size = 5) +
+    geom_point(aes(color = governor, fill = chamber), 
+               size = 6, 
+               pch = 21) +
+    scale_color_identity() +
+    scale_fill_identity()
+  
+  
+  ggsave("figures/test2.png",
+         w = 6, h = 10)
+  
